b7c3ba5963105c8fe1f387a265d80d10be56f693
lawalalx <lawal.alx@gmail.com>
2024-02-21 13:09:03 +0100

    test

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""
-	this is a file where the function prints the methods and attributes
-"""
+"""this is a file where the function prints the methods and attributes"""
 
 def lookup(obj):
-	"""
-		This function prints all methods and list all attributes
-	"""
+	"""This function prints all methods and list all attributes"""
 	return (dir(obj))
