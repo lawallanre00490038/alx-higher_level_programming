09bf63fd538dd16d4e1f56704fc7200d66fb951e
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-04-28 20:58:28 +0100

    test

---
+++ 0x0F-python-object_relational_mapping/data.sql
@@ -0,0 +1,1 @@
+USE hbtn_0e_6_usa;SHOW TABLES
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a python file that contains the class definition of a State
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
