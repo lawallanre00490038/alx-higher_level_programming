b4cda7679915fde7622cd1a2576284699d004c96
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-04-28 16:33:48 +0100

    test

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument and displays all values in the states of hbtn_0e_0_usa"""
+"""Write a script that takes in an argument the states of hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-  db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-  cur = db.cursor()
-  cur.execute("SELECT * FROM states WHERE states.name LIKE '{}'".format(sys.argv[4]))
-  rows = cur.fetchall()
-  for row in rows:
-    print(row)
-  cur.close()
-  db.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name \
+                LIKE '{}'".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
