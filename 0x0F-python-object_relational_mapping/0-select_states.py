a443e8ec526b46d6d479a1ed8b9f551b5fb3fa9b
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-04-28 15:58:12 +0100

    test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-
-"""
-Write a script that lists all states from the database hbtn_0e_0_usa:
-
-Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
-You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-Results must be displayed as they are in the example below
-Your code should not be executed when imported
-"""
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-  import MySQLdb
-  import sys
-
   db = MySQLdb.connect(host="localhost", 
                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
   cur = db.cursor()
-  rows = cur.execute("SELECT * FROM states ORDER BY states.id;")
+  cur.execute("SELECT * FROM states ORDER BY states.id")
   rows = cur.fetchall()
-  
   for row in rows:
     print(row)
   cur.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-  db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
+  db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
   cur = db.cursor()
-  cur.execute("SELECT * FROM states")
+  cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id")
   rows = cur.fetchall()
   for row in rows:
-      print(row)
+    print(row)
   cur.close()
   db.close()
