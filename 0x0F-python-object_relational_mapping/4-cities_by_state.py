a746f34fefdaed63796152164f49fed453fd211a
lawalalx <lawal.alx@gmail.com>
2024-05-03 02:15:29 +0100

    test

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument the states of hbtn_0e_0_usa"""
-import MySQLdb
-import sys
+"""Lists all cities from the database hbtn_0e_4_usa"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("select cities.id, cities.name, states.name\
-                from cities\
-                inner join states\
-                on states.id = cities.id\
-                order by cities.id")
-    rows = cur.fetchall()
-    for row in rows:
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
+
+    if (len(argv) != 4):
+        print('Use: username, password, database name')
+        exit(1)
+
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
+
+    cursor = db.cursor()
+
+    cursor.execute("""SELECT c.id, c.name, s.name FROM cities as c
+                      INNER JOIN states as s
+                      ON c.state_id = s.id
+                      ORDER BY c.id ASC;""")
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
