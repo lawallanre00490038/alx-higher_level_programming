3900121ec2cfca97d1ff5fe3d4a985fcf4d6ec64
lawalalx <lawal.alx@gmail.com>
2024-05-03 02:03:38 +0100

    test

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -8,7 +8,7 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True,
         pool_size=10, max_overflow=20
