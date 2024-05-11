c2b08959fac1af27d5f5135672cea5f058cedc08
lawalalx <lawal.alx@gmail.com>
2024-05-11 14:15:57 +0100

    Test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,6 @@ import urllib.request
 
 if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-
     with urllib.request.urlopen(req) as response:
         html = response.read()
 
