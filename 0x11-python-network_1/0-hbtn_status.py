96d13b8242cb73e4389e34634ead6d4c70b60cfb
lawalalx <lawal.alx@gmail.com>
2024-05-11 14:13:40 +0100

    Test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ import urllib.request
 
 if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    
+
     with urllib.request.urlopen(req) as response:
         html = response.read()
-    
+
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
