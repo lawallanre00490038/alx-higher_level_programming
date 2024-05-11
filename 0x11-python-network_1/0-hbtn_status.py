ef062fb1ed27e20f2528d84bec02d21e4baf737b
lawalalx <lawal.alx@gmail.com>
2024-05-11 14:23:00 +0100

    Test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-A Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+"""a Python script that fetches https://alx-intranet.hbtn.io/status
+requirements: use the package urllib, a with statement
+and have (tabulation before -)"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
+    url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(url) as response:
         content = response.read()
 
     print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    
